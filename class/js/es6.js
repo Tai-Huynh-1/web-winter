270bab023cfc0cd0338700ea98d5e014f1e8c8d7
Tai-Huynh-1 <93554992+Tai-Huynh-1@users.noreply.github.com>
2022-11-30 17:29:03 -0800

    inheritance and other ES6 features

--- class/js/es6.js
+++ class/js/es6.js
@@ -88,17 +88,152 @@ const objB = {};
 // notation: objectToCheck instanceof constructorName
 // console.log(arrB instanceof Array);
 // console.log(objB instanceof Object);
+// Array.isArray(arrB) // static method
 
 // class (ES6)
 class Animal {
-	constructor(name, age) {
-		this.name = name;
-		this.age = age;
-		this.tag = true;
+	// private variable uses # (encapsulation)
+	#type;
+
+	constructor(name, age, type) {
+		this.name = name; // custom properties
+		this.age = age; // custom properties
+		this.#type = type;
+		this.tag = true; // hard-coded properties
+	}
+
+	// static method allows a class method to be called without instantiating the class
+	static logClassName() {
+		console.log("This is the Animal class");
+	}
+
+	// methods
+	greet() {
+		console.log(`Hi, my name is ${this.name} and I am ${this.age} years old.`);
+	}
+
+	// setter & getter methods to modify instance variables (encapsulation)
+	getType() {
+		return this.#type;
+	}
+
+	setType(type) {
+		this.#type = type;
 	}
 }
 
-const tom = new Animal("Tom", 10);
-console.log(tom);
+const tom = new Animal("Tom", 10, "cat");
+// console.log(tom);
+// tom.greet();
+
+// testing encapsulation
+// console.log(tom.getType());
+// tom.setType("dog");
+// console.log(tom.getType());
+
+// Animal.logClassName(); // calling our static method
 
 // inheritance & prototype
+class Vehicle {
+	constructor(brand, year, color, wheels) {
+		this.brand = brand;
+		this.year = year;
+		this.color = color;
+		this.numberOfWheels = wheels;
+	}
+
+	// beep method
+	honk() {
+		console.log("Beep Beep");
+	}
+}
+
+// inheritance by extending properties and methods from a super class
+class SUV extends Vehicle {
+	#type;
+
+	constructor(brand, year, color, wheels) {
+		super(brand, year, color, wheels); // super invokes the constructor from the super class (parent class)
+		this.#type = "suv";
+		this.seats = 7;
+	}
+
+	getSeats() {
+		return this.seats;
+	}
+
+	static getType() {
+		return this.#type;
+	}
+
+	// polymorphism (poly = "many", morphism = "form") - overriding superclass methods
+	honk() {
+		console.log("SUV Honking");
+	}
+}
+
+class Sedan extends Vehicle {
+	#type;
+
+	constructor(brand, year, color, wheels) {
+		super(brand, year, color, wheels);
+		this.#type = "sedan";
+		this.seats = 5;
+	}
+
+	static getType() {
+		return this.#type;
+	}
+}
+
+class Coupe {
+	constructor(brand, year, color, wheels) {
+		this.brand = brand;
+		this.year = year;
+		this.color = color;
+		this.numberOfWheels = wheels;
+	}
+}
+
+const suv1 = new SUV("toyota", 2020, "white", 4);
+suv1.honk();
+
+const coupe1 = new Coupe("toyota", 2000, "white", 4);
+// coupe1.honk();
+
+// ES6 Immediately Invoked Function Expressions (IIFE)
+// (function () {
+// 	console.log("My IIFE");
+// })();
+
+// Default Parameters for functions
+function muliply(a = 1, b = 1) {
+	return a * b;
+}
+
+// Rest parameters for passing in an unspecified amount of arguments
+// Your function packages the arguments into an "args" array
+function restParams(...args) {
+	console.log(args);
+	args.forEach((arg) => console.log(arg));
+}
+// restParams(1, 3, 4, "string", true);
+
+// Spread operator (shallow copy)
+const arrY = [100, 800];
+const arrZ = [1, 2, [44, 55]];
+const arrU = [...arrY, arrZ];
+// console.log(arrU); // [100, 800, [1, 2]]
+// arrZ[0] = 999;
+// console.log(arrU); // [100, 800, [999, 2]]
+// arrY[0] = 555;
+// console.log(arrU); // [100, 800, [999, 2]]
+
+const obj1 = {
+	firstName: "Jack",
+	lastName: "Sparrow",
+};
+const obj2 = { ...obj1 };
+console.log(obj2);
+
+// array & object destructuring
