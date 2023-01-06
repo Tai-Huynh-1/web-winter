c659de4676dd4665f1ab049469df5ebe1b09d887
Tai-Huynh-1 <93554992+Tai-Huynh-1@users.noreply.github.com>
2023-01-05 19:00:12 -0800

    day 34: create and delete todo

--- class/my-app/src/pages/private/NewTodo/index.jsx
+++ class/my-app/src/pages/private/NewTodo/index.jsx
@@ -1,7 +1,71 @@
 import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import PageContainer from "../../../components/layout/PageContainer";
+import useAuth from "../../../hook/useAuth";
 
 const NewTodo = () => {
-	return <div>NewTodo</div>;
+	const [title, setTitle] = useState("");
+	const [desc, setDesc] = useState("");
+	const { user } = useAuth();
+	const navigate = useNavigate();
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		// input validation
+		if (title.length < 3 || desc.length < 3) return;
+
+		const todo = {
+			id: crypto.randomUUID(),
+			title: title.trim(),
+			date: new Date(),
+			owner: user?.name,
+			description: desc.trim(),
+		};
+
+		const currTodos = localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : [];
+
+		currTodos.push(todo);
+
+		localStorage.setItem("todos", JSON.stringify(currTodos));
+		navigate("/todos");
+	};
+
+	const handleReset = (e) => {
+		e.preventDefault();
+		setTitle("");
+		setDesc("");
+	};
+
+	const goBack = () => navigate(-1);
+
+	return (
+		<PageContainer pageTitle={"Add A New Todo"}>
+			<form className="flex flex-col gap-4" onSubmit={handleSubmit} onReset={handleReset}>
+				<div>
+					<label htmlFor="title">Title: </label>
+					<input id="title" onChange={(e) => setTitle(e.target.value)} value={title} className="py-1 px-3 border-2 border-gray-500 rounded-md ml-2" />
+				</div>
+				<div>
+					<label htmlFor="desc">Description: </label>
+					<input id="desc" onChange={(e) => setDesc(e.target.value)} value={desc} className="py-1 px-3 border-2 border-gray-500 rounded-md ml-2" />
+				</div>
+
+				<div className="flex gap-3">
+					<button className="rounded-md border-2 border-transparent bg-indigo-500 text-white py-2 px-3" type="submit">
+						Submit
+					</button>
+					<button className="rounded-md border-2 border-indigo-500 py-2 px-3" type="reset">
+						Reset
+					</button>
+					<button className="rounded-md border-2 border-indigo-500 py-2 px-3" type="button" onClick={goBack}>
+						Back
+					</button>
+				</div>
+			</form>
+		</PageContainer>
+	);
 };
 
 export default NewTodo;
--- class/my-app/src/pages/private/Todos/index.jsx
+++ class/my-app/src/pages/private/Todos/index.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useMemo } from "react";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import PageContainer from "../../../components/layout/PageContainer";
 
 const t = {
@@ -27,7 +28,9 @@ const Todos = () => {
 		};
 	}, []);
 
-	const todoFilter = useMemo(() => todos.filter((todo) => todo.title.includes(filter) || todo.description.includes(filter)), [filter, todos]);
+	const todoFilter = todos.filter((todo) => todo.title.toLowerCase().includes(filter.toLowerCase()) || todo.description.toLowerCase().includes(filter.toLowerCase()));
+
+	// const todoFilter = useMemo(() => todos.filter((todo) => todo.title.includes(filter) || todo.description.includes(filter)), [filter, todos]);
 
 	console.log(todoFilter);
 
@@ -43,6 +46,16 @@ const Todos = () => {
 
 	// continue with building card & link & new todo & individual todo page
 
+	const handleDelete = (idToDelete) => {
+		const todos = localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : null;
+
+		if (!todos) return;
+
+		const newTodos = todos.filter((todo) => todo.id !== idToDelete);
+		localStorage.setItem("todos", JSON.stringify(newTodos));
+		setTodos(newTodos);
+	};
+
 	return (
 		<PageContainer pageTitle={"Todos"}>
 			<div>
@@ -50,11 +63,22 @@ const Todos = () => {
 					<label htmlFor="filter">Search: </label>
 					<input id="filter" className="py-1 px-3 border-2 border-gray-500 rounded-md ml-2" placeholder="filter todos" onChange={(e) => setFilter(e.target.value)} value={filter} />
 				</div>
-				<ul>
-					{todoFilter.map((todo) => (
-						<div key={todo.id}>
-							<h2>{todo.title}</h2>
-							<h3>{todo.description}</h3>
+				<div>
+					<Link to="/todos/new" className="rounded-md bg-indigo-500 text-white py-2 px-3">
+						Create New Todo
+					</Link>
+				</div>
+
+				<ul className="mt-6 flex flex-col gap-2">
+					{todoFilter.map(({ id, title, description }) => (
+						<div className="border-gray-300 border-2 rounded-md p-4" key={id}>
+							<Link to={`/todos/${id}`}>
+								<h2 className="text-center font-bold text-xl">Title: {title}</h2>
+								<h3>Description: {description}</h3>
+							</Link>
+							<button className="rounded-md bg-red-500 text-white py-2 px-3" onClick={() => handleDelete(id)}>
+								Delete
+							</button>
 						</div>
 					))}
 				</ul>
