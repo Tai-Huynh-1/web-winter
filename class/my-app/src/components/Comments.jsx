dd80824bef1ed59e271feb0b7cc7dc9dedce8af4
Tai-Huynh-1 <93554992+Tai-Huynh-1@users.noreply.github.com>
2023-01-10 16:04:44 -0800

    implement test for Comments component

--- class/my-app/src/components/Comments.jsx
+++ class/my-app/src/components/Comments.jsx
@@ -1,52 +1,51 @@
-import React from "react";
 import { useState, useEffect } from "react";
-import axios from "axios";
+// import axios from "axios";
 
 const Comments = () => {
 	const [data, setData] = useState([]);
 	const [err, setErr] = useState(false);
 
 	// cover arrow functions curly vs parenthesis
-	// useEffect(() => {
-	// 	console.log("fetching data");
-	// 	fetch("https://jsonplaceholder.typicode.com/comments")
-	// 		.then((response) => {
-	// 			console.log("response", response);
-	// 			return response.json();
-	// 		})
-	// 		.then((result) => {
-	// 			setData(result.slice(0, 10));
-	// 		})
-	// 		.catch((err) => {
-	// 			console.log("err", err);
-	// 			setErr(true);
-	// 		});
-
-	// 	console.log("after fetch");
-	// }, []);
-
 	useEffect(() => {
-		let isMounted = true;
+		console.log("fetching data");
+		fetch("https://jsonplaceholder.typicode.com/comments")
+			.then((response) => {
+				console.log("response", response);
+				return response.json();
+			})
+			.then((result) => {
+				setData(result.slice(0, 10));
+			})
+			.catch((err) => {
+				console.log("err", err);
+				setErr(true);
+			});
 
-		async function fetchComments() {
-			const url = "https://jsonplaceholder.typicode.com/comments";
-			try {
-				const { data } = await axios.get(url);
-				console.log("setting data");
-				isMounted && setData(data.slice(0, 10));
-			} catch (err) {
-				console.log(err);
-				setErr(err);
-			}
-		}
-		console.log("before");
-		fetchComments();
-		console.log("after");
-
-		return () => {
-			isMounted = false;
-		};
+		console.log("after fetch");
 	}, []);
+
+	// useEffect(() => {
+	// 	let isMounted = true;
+
+	// 	async function fetchComments() {
+	// 		const url = "https://jsonplaceholder.typicode.com/comments";
+	// 		try {
+	// 			const { data } = await axios.get(url);
+	// 			console.log("setting data");
+	// 			isMounted && setData(data.slice(0, 10));
+	// 		} catch (err) {
+	// 			console.log(err);
+	// 			setErr(err);
+	// 		}
+	// 	}
+	// 	console.log("before");
+	// 	fetchComments();
+	// 	console.log("after");
+
+	// 	return () => {
+	// 		isMounted = false;
+	// 	};
+	// }, []);
 
 	if (err) {
 		return (
@@ -62,10 +61,10 @@ const Comments = () => {
 			<h1>Comments</h1>
 			<ul style={{ border: "1px solid black" }}>
 				{data.map((comment) => (
-					<div key={comment.id}>
+					<li key={comment.id}>
 						<h2>{comment.name}</h2>
 						<p>{comment.body}</p>
-					</div>
+					</li>
 				))}
 			</ul>
 		</div>
--- class/my-app/src/components/Comments.test.jsx
+++ class/my-app/src/components/Comments.test.jsx
@@ -1,12 +1,38 @@
 import Comments from "./Comments";
 import { render } from "@testing-library/react";
+import { server } from "../mocks/server";
+import { rest } from "msw";
 
 describe("Comments Component", () => {
 	describe("Successful API Call", () => {
-		it("should render", () => {
+		it("should render a list of comments", async () => {
+			// ARRANGE (setup: render the component to the vDOM)
 			const screen = render(<Comments />);
+
+			// ACT (perform some action on the UI to imitate user response)
+			const commentsList = await screen.findAllByRole("listitem");
+
+			// ASSERT (compare results vs. our expectation)
+			expect(commentsList).toHaveLength(3);
 		});
 	});
 
-	describe("Failed API Call");
+	describe("Failed API Call", () => {
+		it("should display an error message", async () => {
+			// ARRANGE (reset & override the "get" behavior for this route to return an error instead)
+			server.resetHandlers(
+				rest.get("https://jsonplaceholder.typicode.com/comments", (req, res, ctx) => {
+					return res(ctx.status(500));
+				})
+			);
+
+			const screen = render(<Comments />);
+
+			// ACT (user waits for component to mount and fetch data)
+			const errMsg = await screen.findByText("An unexpected error occured!");
+
+			// ASSERT
+			expect(errMsg).toBeInTheDocument();
+		});
+	});
 });
