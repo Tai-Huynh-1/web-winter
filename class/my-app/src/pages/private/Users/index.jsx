1d4af11abc32e0272a677dd708e8f1ed4ebac37e
Tai-Huynh-1 <93554992+Tai-Huynh-1@users.noreply.github.com>
2023-01-12 15:59:31 -0800

    finished select user dropdown

--- class/my-app/src/pages/private/Users/index.jsx
+++ class/my-app/src/pages/private/Users/index.jsx
@@ -1,7 +1,6 @@
-import { useEffect } from "react";
-import PageContainer from "../../../components/layout/PageContainer";
+import { useEffect, useState, useMemo } from "react";
 import axios from "axios";
-import { useState } from "react";
+import PageContainer from "../../../components/layout/PageContainer";
 
 // const u = {
 //     email: "",
@@ -12,21 +11,30 @@ import { useState } from "react";
 //     website: "",
 // }
 
-const Card = (props) => {
+const Card = ({ user }) => {
+	const { name, username, email, phone, website } = user;
+	const initials = useMemo(
+		() =>
+			name
+				.split(" ")
+				.map((name) => name[0])
+				.join(""),
+		[name]
+	);
 	return (
-		<div className="bg-slate-500 text-white">
-			<div>
-				<div>A</div>
-				<div>
-					<p>Name</p>
-					<p>Username</p>
+		<div className="bg-slate-700 text-white flex flex-col p-6 gap-4 w-11/12 mx-auto rounded-md">
+			<div className="flex gap-4">
+				<span className={`bg-white text-indigo-500 ${initials.length < 3 ? "text-3xl" : "text-2xl"} font-semibold rounded-full w-14 h-14 flex justify-center items-center`}>{initials}</span>
+				<div className="">
+					<p>{name}</p>
+					<p className="text-stone-300">Username: {username}</p>
 				</div>
 			</div>
 
 			<div>
-				<p>Phone</p>
-				<p>Email</p>
-				<p>Website</p>
+				<p>Phone: {phone}</p>
+				<p>Email: {email}</p>
+				<p>Website: {website}</p>
 			</div>
 		</div>
 	);
@@ -37,7 +45,6 @@ const Users = () => {
 	const [selected, setSelected] = useState("");
 	const [selectedUser, setSelectedUser] = useState(null);
 
-	console.log(users);
 	useEffect(() => {
 		let isMounted = true;
 
@@ -50,21 +57,26 @@ const Users = () => {
 		};
 	}, []);
 
-	useEffect(() => {
-		const found = users.find((u) => u.name === selected);
+	const handleSelect = (e) => {
+		setSelected(e.target.value);
 
+		const found = users.find((u) => u.name === e.target.value);
 		if (found) {
 			setSelectedUser(found);
 		} else {
 			setSelectedUser(null);
 		}
-	}, [selected, users]);
+	};
+
+	console.log(users);
+	console.log("selected", selected);
+	console.log("selected User", selectedUser);
 
 	return (
 		<PageContainer pageTitle="Users">
-			<div className="flex flex-col items-center gap-4">
-				<select className="border-2 border-black rounded-md py-2 px-2" onChange={(e) => setSelected(e.target.value)} value={selected}>
-					<option value="" disabled>
+			<div className="flex flex-col gap-8 w-8/12 mx-auto">
+				<select className="border-2 border-black rounded-md py-2 px-2" onChange={handleSelect} value={selected}>
+					<option value="" disabled hidden>
 						Please Select A User
 					</option>
 					{users.map(({ id, name }) => (
@@ -73,7 +85,7 @@ const Users = () => {
 						</option>
 					))}
 				</select>
-				<div>{selectedUser ? <Card /> : <p>Please Select A User</p>}</div>
+				<div>{selectedUser && <Card user={selectedUser} />}</div>
 			</div>
 		</PageContainer>
 	);
