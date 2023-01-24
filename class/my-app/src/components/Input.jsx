caf83e5644c4f587132b495699c5fb7e6c07ecfe
Tai-Huynh-1 <93554992+Tai-Huynh-1@users.noreply.github.com>
2023-01-24 15:59:43 -0800

    Update Input.jsx

--- class/my-app/src/components/Input.jsx
+++ class/my-app/src/components/Input.jsx
@@ -1,41 +1,59 @@
-import { Component, Fragment } from "react";
+// import { Component, Fragment } from "react";
 
-// function Input() {
-// 	return <input />;
+// // function Input() {
+// // 	return <input />;
+// // }
+
+// /**
+//  * A controlled Input component. (Controlled component)
+//  * The value of input is tied to react's state. onChange will update that state.
+//  */
+// class Input extends Component {
+// 	constructor(props) {
+// 		super(props);
+// 		this.state = {
+// 			text: "",
+// 		};
+
+// 		this.handleChange = this.handleChange.bind(this);
+// 	}
+
+// 	/**
+// 	 * This method updates the "text" state when an event is emitted.
+// 	 * @param {*} event
+// 	 */
+// 	handleChange(event) {
+// 		this.setState({
+// 			text: event.target.value,
+// 		});
+// 	}
+
+// 	render() {
+// 		return (
+// 			<Fragment>
+// 				<h1>Text: {this.state.text}</h1>
+// 				<input type="text" value={this.state.text} onChange={this.handleChange} />
+// 			</Fragment>
+// 		);
+// 	}
 // }
 
-/**
- * A controlled Input component. (Controlled component)
- * The value of input is tied to react's state. onChange will update that state.
- */
-class Input extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			text: "",
-		};
+// export default Input;
 
-		this.handleChange = this.handleChange.bind(this);
-	}
+import React from "react";
 
-	/**
-	 * This method updates the "text" state when an event is emitted.
-	 * @param {*} event
-	 */
-	handleChange(event) {
-		this.setState({
-			text: event.target.value,
-		});
-	}
+const Input = ({ id, className, secondary, label, error, ...props }) => {
+	const styles = `${secondary ? "secondary-style" : "primary-style"} ${className}`;
 
-	render() {
-		return (
-			<Fragment>
-				<h1>Text: {this.state.text}</h1>
-				<input type="text" value={this.state.text} onChange={this.handleChange} />
-			</Fragment>
-		);
-	}
-}
+	return (
+		<>
+			<label id={id}>
+				{label}:
+				<input id={id} className={styles} type="text" {...props} />
+			</label>
+			{error && <p>{error}</p>}
+		</>
+	);
+};
 
 export default Input;
